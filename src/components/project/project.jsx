07b3f74f057a38e34e12b4f01b6e6c16f4c98f78
Danton Ruthe <danton.ruthe@gmail.com>
2019-05-06 18:50:25 +1200

    Added project component

---
+++ src/components/project/project.jsx
@@ -0,0 +1,15 @@
+class Project extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <h1>This is the Project Component</h1>
+      </div>
+    );
+  }
+}
+
+export default Project;
